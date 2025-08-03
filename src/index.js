035f3f855635d4bd917e53547bdbd9f0de36b11e
Mpho Matli <mphojames9@gmail.com>
2025-08-03 15:09:08 +0200

    Add DOM element references for form input validation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Hello from Webpack!');
+const form = document.getElementById("registrationForm");
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const phone = document.getElementById("phone");
+  const password = document.getElementById("password");
+  const confirmPassword = document.getElementById("confirmPassword");
+
+  const nameError = document.getElementById("nameError");
+  const emailError = document.getElementById("emailError");
+  const phoneError = document.getElementById("phoneError");
+  const passwordError = document.getElementById("passwordError");
+  const confirmPasswordError = document.getElementById("confirmPasswordError");
+  const successMessage = document.getElementById("successMessage");
